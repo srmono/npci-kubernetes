e0acf07674369ebae3d87ef5f7a8ca67fb658484
srmono <bvsrao91@gmail.com>
2022-03-14 05:48:11 +0530

    K8S Lab Commit

---
+++ springboot-crud/src/test/java/com/npci/crud/SpringbootCrudK8sExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.npci.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudK8sExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
